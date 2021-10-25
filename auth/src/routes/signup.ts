dd1a78478ef5c695e8c78dd7ca9051a14b8ec6da
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-10-25 09:34:57 -0700

    Added RequestValidationError and DatabaseConnectionError to handle error from request and database

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import {body, validationResult} from 'express-validator'
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 
 const router = express.Router();
 
@@ -13,9 +16,14 @@ router.post('/api/users/signup', [
     .withMessage('Password must be between 4 and 20 characters')
 ],   (req: Request, res: Response) => {
     const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        throw new RequestValidationError(errors.array())
+    }
+    
+    console.log("Creating a user...");
 
-    if(!errors.isEmpty()) return res.status(400).send(errors.array())
 
+    throw new DatabaseConnectionError()
 
   const {email, password} = req.body
 
