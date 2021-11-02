b76419be1fdcde4f3517f95c54d44b00856c8b13
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-02 04:21:04 -0700

    Added a logic to check JWT before mongoDB connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,45 +1,56 @@
 import express from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
 import { json } from 'body-parser';
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+
+//The reason is traffic is being proxied to our application through
+//ingress-nginx. Express will see the fact is being proxied and by default it will say there is a proxy
+//It should trust traffic that it secure even though it is coming from that proxy
+app.set('trust proxy', true);
+
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all("*", async (req, res,) => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
-
 app.use(errorHandler);
 
-  const start = async() => {
-    try {
-  await mongoose.connect('mongodb://auth-mongo-srv:27017/auth') 
-      console.log('Connected to mongoDB')
-    } catch (err) {
-      console.log(err)
-    }
-    app.listen(3000, () => {
-      console.log('Listening on port 3000!!!');
-    });
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
   }
 
-  start()
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to mongoDB');
+  } catch (err) {
+    console.log(err);
+  }
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!');
+  });
+};
 
-
-
-
-
+start();
