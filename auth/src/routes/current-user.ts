d59492e3d0c03fdbea3e2656bcf0449f5888ab2f
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-02 04:42:19 -0700

    Added a logic to extract information of a current-user stored in jwt

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('Current user');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  //jwt.verify is how we decode or extract information out of jwt
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
