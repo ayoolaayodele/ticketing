795b98520e29cf5cc629823faebfa63db805609c
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-03 01:57:22 -0700

    Refactored file to serve app.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-
-//The reason is traffic is being proxied to our application through
-//ingress-nginx. Express will see the fact is being proxied and by default it will say there is a proxy
-//It should trust traffic that it secure even though it is coming from that proxy
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -44,12 +9,13 @@ const start = async () => {
 
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to mongoDB');
+    console.log('Connected to MongoDb');
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
+
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!');
+    console.log('Listening on port 3000!!!!!!!!');
   });
 };
 
