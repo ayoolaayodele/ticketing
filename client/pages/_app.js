256ee85c9703e0beaf0ef16da0e307126f7e679a
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-06 13:16:52 -0700

    Implemented a logic for handling multiple getInitialprops across multiple pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/buildClient';
+import Header from '../components/Header';
 
-const _app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default _app;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  //passing data to multiple pages
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
