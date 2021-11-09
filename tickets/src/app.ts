57d0f95cd46c1bac61be4726da5e31bea74fcb72
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-09 01:27:32 -0800

    Imported routes and handler to server

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,19 +2,30 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@aytickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@aytickets/common';
 import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
+//add currentUser middleware after cookie session,
+//cookie has to run first to take a look at the cookie and set the req.session property
+
 app.use(
   cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
