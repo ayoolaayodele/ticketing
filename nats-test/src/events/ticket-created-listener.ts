2b50ccb2270dff9120cee3d47c3fe574bec3227b
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-18 02:03:05 -0800

    Reduced the number of times plain strings are referred inside of listener Implemented a logic to make sure that we only

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -4,10 +4,10 @@ import { TicketCreatedEvent } from './ticket-created-event';
 import { Subjects } from './subjects';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated; //Typescript thinks we might try to change
+  readonly subject = Subjects.TicketCreated; //Typescript thinks we might try to change
   //the value of subjects//to make sure we can never change the value of TS
-  queueGroupName = 'payments-service';
 
+  queueGroupName = 'payments-service';
   onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
 
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,5 @@
 import nats from 'node-nats-streaming';
-import { randomBytes } from 'crypto';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -10,13 +10,24 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
+  stan.on('connect', () => {
+    console.log('Publisher connected to NATS');
+  });
+
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
     id: '123',
     title: 'concert',
     price: 20,
   });
 
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'concert',
+  //   price: '$20',
+  // });
+
+  // stan.publish('TicketCreated', data, () => {
+  //   console.log('Event published');
+  // });
 });
