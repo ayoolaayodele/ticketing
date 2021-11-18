300a5691dfa5b17b61b8bf11742a6b35097a7065
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-18 00:56:02 -0800

    Reduced the number of times plain strings are referred inside of listener

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated; //Typescript thinks we might try to change
+  //the value of subjects//to make sure we can never change the value of TS
   queueGroupName = 'payments-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data);
 
     msg.ack();
