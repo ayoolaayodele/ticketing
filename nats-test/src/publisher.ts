f18fb1694515c557af50ae88497adad6013656f5
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-18 02:15:05 -0800

    Implemented Promise to await event publication

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,7 +7,7 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
   stan.on('connect', () => {
@@ -15,12 +15,15 @@ stan.on('connect', () => {
   });
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 20,
-  });
-
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
   // const data = JSON.stringify({
   //   id: '123',
   //   title: 'concert',
