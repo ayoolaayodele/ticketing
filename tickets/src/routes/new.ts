ffcfe3c3802fd6edbf684f68a24b70ea0a111ba2
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-20 14:41:04 -0800

    Imported TicketCreatedPublisher and natsWrapper to publish event created to any other service listening

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@aytickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publisher/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -11,7 +13,6 @@ router.post(
   [
     body('title').not().isEmpty().withMessage('Title is required'),
     body('price')
-      //A float is a number that has a decimal component
       .isFloat({ gt: 0 })
       .withMessage('Price must be greater than 0'),
   ],
@@ -25,6 +26,12 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
