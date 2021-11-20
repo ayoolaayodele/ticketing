7e913f486d54027dc9f440ee9511d8a6502ba8e3
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-20 14:48:45 -0800

    Implemented a logic to make sure we refer to the correct property inside incoming data

---
+++ tickets/src/events/publisher/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from '@aytickets/common';
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
