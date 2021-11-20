d0cc00ec21f19e218dde076b828c48a724804d54
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-20 09:54:34 -0800

    Modified file

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import { currentUser } from '@aytickets/common';
 
 const router = express.Router();
-
 router.get('/api/users/currentuser', currentUser, (req, res) => {
   res.send({ currentUser: req.currentUser || null });
 });
