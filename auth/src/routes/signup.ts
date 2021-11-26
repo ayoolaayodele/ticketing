c885e0c720d5e77d9de2b693b854cbee391e0bc3
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-26 15:31:20 -0800

    Modified file

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
 import { validateRequest, BadRequestError } from '@aytickets/common';
+
 import { User } from '../models/user';
 
 const router = express.Router();
@@ -28,7 +29,7 @@ router.post(
     const user = User.build({ email, password });
     await user.save();
 
-    //Generate JWT
+    // Generate JWT
     const userJwt = jwt.sign(
       {
         id: user.id,
@@ -37,7 +38,7 @@ router.post(
       process.env.JWT_KEY!
     );
 
-    //Store it on session object
+    // Store it on session object
     req.session = {
       jwt: userJwt,
     };
