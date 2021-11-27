ef442eea5c6c890bb09f10ef370c625d200bd470
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-26 16:22:23 -0800

    Implemented ticketCreatedListener to listen and acknowledged published event

---
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,23 @@
+import { Message } from 'node-nats-streaming';
+import { Subjects, Listener, TicketCreatedEvent } from '@aytickets/common';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = Ticket.build({
+      id,
+      title,
+      price,
+    });
+    await ticket.save();
+
+    //To know notify nat-streaming server we are done processing the message
+    msg.ack();
+  }
+}
