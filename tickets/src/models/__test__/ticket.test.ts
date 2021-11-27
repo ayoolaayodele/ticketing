8190de4cf700fbbb8acc69c0f85b7d3004a3dd4b
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-26 16:25:47 -0800

    Implemented automated test to test optimistic concurrency control and a test logic to check if the version gets updated by 1 on save

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,50 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+
+  // Save the ticket to the database
+  await ticket.save();
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // make two separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+
+  // save the first fetched ticket
+
+  await firstInstance!.save(); // We will expect the first fetched ticket to work  because it has version 1
+
+  // save the second fetched ticket and expect an error because the version 0 has been outdated
+  try {
+    await secondInstance!.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point');
+});
+
+//To check if the version gets updated by 1 on save
+it('increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
