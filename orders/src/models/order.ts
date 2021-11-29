d392de62aaf1593a47d225413895b647722c0285
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-29 10:19:13 -0800

    Imported installed mongoose-update-if-current to solve concurrency when emitting events through automatic updates of mongoose version

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 import { OrderStatus } from '@aytickets/common';
 import { TicketDoc } from './ticket';
 
@@ -20,6 +21,7 @@ interface OrderDoc extends mongoose.Document {
   status: OrderStatus;
   expiresAt: Date;
   ticket: TicketDoc;
+  version: number;
 }
 
 //An interface that describes the properties
@@ -57,6 +59,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
