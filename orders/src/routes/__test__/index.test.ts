d7288f09d2a3011f60f26de5e9eb219a68606775
Ayoola Ayodele <ayodeleayoola264@gmail.com>
2021-11-29 10:24:13 -0800

    Added id to tickets model

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
 import { Order } from '../../models/order';
@@ -7,6 +8,7 @@ import { Ticket } from '../../models/ticket';
 //ticket in test environment
 const buildTicket = async () => {
   const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   });
